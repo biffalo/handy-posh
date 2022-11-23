53263273099d16e1eb5bda0098f264cbc419bb8f
Biff Tannen <biffalo@protonmail.com>
2022-11-23 07:41:37 -0500

    Update disable-iso-mount.ps1

--- disable-iso-mount.ps1
+++ disable-iso-mount.ps1
@@ -1,1 +1,2 @@
+#disables mounting of ISOs via file explorer. thanks to https://support.huntress.io/hc/en-us/articles/11477430445587-How-to-disable-ISO-mounting#
 reg add "HKEY_CLASSES_ROOT\Windows.IsoFile\shell\mount" /v "ProgrammaticAccessOnly" /t REG_SZ /d no /f
