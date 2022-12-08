86b77831b775f0b8a57b08f7503ba3048aacdb0f
Biff Tannen <biffalo@protonmail.com>
2022-12-08 14:19:30 -0500

    Update windows_hardening.cmd
    ripped out logging section to roll as standalone script. removed some settings that break lower AD functional level environments.

--- windows_hardening.cmd
+++ windows_hardening.cmd
@@ -29,15 +29,7 @@ reg add "HKCU\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Main" /v "FormSuggest Pa
 ::
 ::#######################################################################
 :: Enable and Configure General Windows Security Settings
-::#######################################################################S
-::
-:: Prioritize ECC Curves with longer keys
-::reg add "HKLM\SOFTWARE\Policies\Microsoft\Cryptography\Configuration\SSL\00010002" /v EccCurves /t REG_MULTI_SZ /d NistP384,NistP256 /f
-:: Prevent Kerberos from using DES or RC4
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\Kerberos\Parameters" /v SupportedEncryptionTypes /t REG_DWORD /d 2147483640 /f
-:: Encrypt and sign outgoing secure channel traffic when possible
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters" /v SealSecureChannel /t REG_DWORD /d 1 /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters" /v SignSecureChannel /t REG_DWORD /d 1 /f
+::#######################################################################
 ::
 :: Enable SmartScreen
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v EnableSmartScreen /t REG_DWORD /d 1 /f
@@ -139,38 +131,3 @@ reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" 
 reg add "HKCU\Control Panel\International\User Profile" /v HttpAcceptLanguageOptOut /t REG_DWORD /d 1 /f
 :: Prevent toast notifications from appearing on lock screen
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications" /v NoToastApplicationNotificationOnLockScreen /t REG_DWORD /d 1 /f
-::
-::#######################################################################
-:: Enable Advanced Windows Logging
-::#######################################################################
-::
-:: Enlarge Windows Event Security Log Size
-wevtutil sl Security /ms:1024000
-wevtutil sl Application /ms:1024000
-wevtutil sl System /ms:1024000
-wevtutil sl "Windows Powershell" /ms:1024000
-wevtutil sl "Microsoft-Windows-PowerShell/Operational" /ms:1024000
-:: Record command line data in process creation events eventid 4688
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\Audit" /v ProcessCreationIncludeCmdLine_Enabled /t REG_DWORD /d 1 /f
-::
-:: Enabled Advanced Settings
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v SCENoApplyLegacyAuditPolicy /t REG_DWORD /d 1 /f
-:: Enable PowerShell Logging
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging" /v EnableModuleLogging /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" /v EnableScriptBlockLogging /t REG_DWORD /d 1 /f
-::
-:: Enable Windows Event Detailed Logging
-:: This is intentionally meant to be a subset of expected enterprise logging as this script may be used on consumer devices.
-:: For more extensive Windows logging, I recommend https://www.malwarearchaeology.com/cheat-sheets
-Auditpol /set /subcategory:"Security Group Management" /success:enable /failure:enable
-Auditpol /set /subcategory:"Process Creation" /success:enable /failure:enable
-Auditpol /set /subcategory:"Logoff" /success:enable /failure:disable
-Auditpol /set /subcategory:"Logon" /success:enable /failure:enable 
-Auditpol /set /subcategory:"Filtering Platform Connection" /success:enable /failure:disable
-Auditpol /set /subcategory:"Removable Storage" /success:enable /failure:enable
-Auditpol /set /subcategory:"SAM" /success:disable /failure:disable
-Auditpol /set /subcategory:"Filtering Platform Policy Change" /success:disable /failure:disable
-Auditpol /set /subcategory:"IPsec Driver" /success:enable /failure:enable
-Auditpol /set /subcategory:"Security State Change" /success:enable /failure:enable
-Auditpol /set /subcategory:"Security System Extension" /success:enable /failure:enable
-Auditpol /set /subcategory:"System Integrity" /success:enable /failure:enable
