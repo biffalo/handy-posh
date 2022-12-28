264f498d90412142dc0626b0491ca3a8a9a90242
Biff Tannen <biffalo@protonmail.com>
2022-12-28 14:17:46 -0500

    Add files via upload

---
+++ malicious-link-finder.ps1
@@ -0,0 +1,32 @@
+# Set the root directory to search
+$root = "C:\users"
+
+# Get all the .LNK files in the root directory and its subdirectories
+$lnkFiles = Get-ChildItem $root -Filter "*.lnk" -Recurse
+
+# Iterate through each .LNK file
+foreach ($lnkFile in $lnkFiles)
+{
+    # Get the target of the .LNK file
+    $target = (New-Object -ComObject WScript.Shell).CreateShortcut($lnkFile.FullName).TargetPath
+
+    # Check if the target contains rundll
+    if ($target -like "*rundll32.exe*")
+    {
+        # Print the full path of the .LNK file and its target
+        Write-Host "$($lnkFile.FullName) -> $target"
+    }
+}
+
+foreach ($lnkFile in $lnkFiles)
+{
+    # Get the target of the .LNK file#
+    $target = (New-Object -ComObject WScript.Shell).CreateShortcut($lnkFile.FullName).TargetPath
+
+    # Check if the target contains cmd.exe#
+    if ($target -like "*cmd.exe*")
+    {
+        # Print the full path of the .LNK file and its target#
+        Write-Host "$($lnkFile.FullName) -> $target"
+    }
+}
