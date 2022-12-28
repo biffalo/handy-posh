3ef81072877f91dc908d58b27f1b03f877384b2a
Biff Tannen <biffalo@protonmail.com>
2022-12-28 13:30:37 -0500

    Update windows_hardening.cmd

--- windows_hardening.cmd
+++ windows_hardening.cmd
@@ -34,7 +34,7 @@ reg add "HKCU\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Main" /v "FormSuggest Pa
 ::
 :: Enable SmartScreen
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v EnableSmartScreen /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v ShellSmartScreenLevel /t REG_SZ /d Block /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v ShellSmartScreenLevel /t REG_SZ /d Warn /f
 ::
 ::
 :: Windows Update Settings
