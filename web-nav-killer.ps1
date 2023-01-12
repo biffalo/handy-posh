9d8c3f2a81ed03f3b21bc9759dcb3a3a62c4c44d
Biff Tannen <biffalo@protonmail.com>
2023-01-12 07:37:07 -0500

    Update web-nav-killer.ps1
    Added additional folders and additional scheduled task

--- web-nav-killer.ps1
+++ web-nav-killer.ps1
@@ -1,19 +1,28 @@
-#this script removes persistence objects and binaries for the "Web Companion" adware#
+#this script removes persistence objects and binaries for the "Web Companion/WebNavigator" adware#
 #removes scheduled task#
 Unregister-ScheduledTask -TaskName "WC ScheduledTask" -Confirm:$false
+Unregister-ScheduledTask -TaskName "BB Updater Scheduler" -Confirm:$false
 #remove folder for each user on system#
 #Get all users profiles
 $users = Get-ChildItem "C:\Users" -Directory 
-#Iterate through each user
+#Iterate through each user and yeet
 foreach ($user in $users) {
     #construct the path for the folders to delete
     $folder1 = "$($user.FullName)\AppData\Roaming\BBWC"
     $folder2 = "$($user.FullName)\AppData\Roaming\Browser Extension"
+    $folder3 = "$($user.FullName)\AppData\Local\WebNavigatorBrowser"
+    $folder4 = "$($user.FullName)\AppData\Local\BlazeMedia"
     # check if the folder exist and then remove them
     if (Test-Path $folder1) {
         Remove-Item $folder1 -Recurse -Force
     }
     if (Test-Path $folder2) {
         Remove-Item $folder2 -Recurse -Force
+    }
+    if (Test-Path $folder3) {
+        Remove-Item $folder3 -Recurse -Force
+    }
+    if (Test-Path $folder4) {
+        Remove-Item $folder4 -Recurse -Force
     }
 }
