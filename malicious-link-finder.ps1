22af2fbc42033437cde2a23224dd9b649f71b084
Biff Tannen <biffalo@protonmail.com>
2023-01-12 08:48:41 -0500

    Update malicious-link-finder.ps1
    fixed returning legit posh and cmd lnks via . Suppressed errors for paths being too long since that isn't fixable.

--- malicious-link-finder.ps1
+++ malicious-link-finder.ps1
@@ -1,8 +1,11 @@
+#Malicious LNK Finder#
+#Parses every LNK file in C:\users and spits out any where target is like rundll32/cmd/powershell#
 # Set the root directory to search
 $root = "C:\users"
-
-# Get all the .LNK files in the root directory and its subdirectories
-$lnkFiles = Get-ChildItem $root -Filter "*.lnk" -Recurse
+$ErrorActionPreference= 'silentlycontinue'
+# Get all the .LNK files in the root directory and its subdirectories, skipping legit powershell and cmd shortcuts#
+$excluded = @("*Command Prompt.lnk", "*Windows PowerShell.lnk", "*Windows PowerShell (x86).lnk")
+$lnkFiles = Get-ChildItem $root -Filter "*.lnk" -Recurse -Exclude $excluded
 
 # Iterate through each .LNK file
 foreach ($lnkFile in $lnkFiles)
@@ -11,10 +14,10 @@ foreach ($lnkFile in $lnkFiles)
     $target = (New-Object -ComObject WScript.Shell).CreateShortcut($lnkFile.FullName).TargetPath
 
     # Check if the target contains rundll
-    if ($target -like "*rundll32.exe*")
+    if ($target -like "*rundll32*")
     {
         # Print the full path of the .LNK file and its target
-        Write-Host "$($lnkFile.FullName) -> $target"
+        Write-Host "LNK PATH: $($lnkFile.FullName) -> LNK TARGET: $target"
     }
 }
 
@@ -24,10 +27,10 @@ foreach ($lnkFile in $lnkFiles)
     $target = (New-Object -ComObject WScript.Shell).CreateShortcut($lnkFile.FullName).TargetPath
 
     # Check if the target contains cmd.exe#
-    if ($target -like "*cmd.exe*")
+    if ($target -like "*cmd*")
     {
         # Print the full path of the .LNK file and its target#
-        Write-Host "$($lnkFile.FullName) -> $target"
+        Write-Host "LNK PATH: $($lnkFile.FullName) -> LNK TARGET: $target"
     }
 }
 foreach ($lnkFile in $lnkFiles)
@@ -36,9 +39,9 @@ foreach ($lnkFile in $lnkFiles)
     $target = (New-Object -ComObject WScript.Shell).CreateShortcut($lnkFile.FullName).TargetPath
 
     # Check if the target contains powershell.exe#
-    if ($target -like "*powershell.exe*")
+    if ($target -like "*powershell*")
     {
         # Print the full path of the .LNK file and its target#
-        Write-Host "$($lnkFile.FullName) -> $target"
+        Write-Host "LNK PATH: $($lnkFile.FullName) -> LNK TARGET: $target"
     }
 }
