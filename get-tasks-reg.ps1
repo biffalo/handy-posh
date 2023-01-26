d165ed62e4715669f7d4fad566838c619d7b72e6
Biff Tannen <biffalo@protonmail.com>
2023-01-26 16:37:14 -0500

    Update get-tasks-reg.ps1

--- get-tasks-reg.ps1
+++ get-tasks-reg.ps1
@@ -1,4 +1,4 @@
-#gets actions of every sch task in registry#
+#gets actions of every sch task in registry NOTE: requires winreg-tasks exe to be in same folder as script https://github.com/GDATAAdvancedAnalytics/winreg-tasks#
 #tasks var gets last 38 characters of the get-child-item | select-object command which is = to full task guid including {} #
 $tasks = Get-ChildItem -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Schedule\Taskcache\Tasks\" | Select-Object -ExpandProperty Name | ForEach-Object { $_.Substring($_.Length - 38)}
 foreach ($task in $tasks) {
