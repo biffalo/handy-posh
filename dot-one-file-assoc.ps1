15c7dc6b6b0204349a0259a4f24b69292b63c924
Biff Tannen <biffalo@protonmail.com>
2023-01-26 11:09:55 -0500

    Add files via upload

---
+++ dot-one-file-assoc.ps1
@@ -0,0 +1,14 @@
+#sets .one files to open in notepad for each user#
+$users = Get-ChildItem Registry::HKU
+foreach ($user in $users) {
+        $key = "Registry::\$($user.Name)\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.one\OpenWithList"
+            Set-ItemProperty -Path $key -Name "a" -Value "Microsoft.WindowsNotepad_8wekyb3d8bbwe!App" -Force -ErrorAction SilentlyContinue
+            Set-ItemProperty -Path $key -Name "b" -Value "Microsoft.WindowsNotepad_8wekyb3d8bbwe!App" -Force -ErrorAction SilentlyContinue
+            Set-ItemProperty -Path $key -Name "MRUList" -Value "ba" -Force -ErrorAction SilentlyContinue
+        }
+foreach ($user in $users) {
+        $key = "Registry::\$($user.Name)\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.one\UserChoice"
+            Get-Item -Path $key | Remove-Item -Force -Verbose
+            Remove-ItemProperty -Path $key -Name "ProgID" -Force
+            Remove-ItemProperty -Path $key -Name "Hash" -Force 
+            }
