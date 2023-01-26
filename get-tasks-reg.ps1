32c38dc4e64a4b9af1aa34216306fc0203e85978
Biff Tannen <biffalo@protonmail.com>
2023-01-26 16:35:57 -0500

    Add files via upload

---
+++ get-tasks-reg.ps1
@@ -0,0 +1,6 @@
+#gets actions of every sch task in registry#
+#tasks var gets last 38 characters of the get-child-item | select-object command which is = to full task guid including {} #
+$tasks = Get-ChildItem -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Schedule\Taskcache\Tasks\" | Select-Object -ExpandProperty Name | ForEach-Object { $_.Substring($_.Length - 38)}
+foreach ($task in $tasks) {
+        .\winreg-tasks-amd64.exe actions "$task"
+        }
