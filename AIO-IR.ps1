df50ad0a21bb0671b5f0fd3b7a9b5ed827b1fc87
Biff Tannen <biffalo@protonmail.com>
2023-01-27 15:15:53 -0500

    Update AIO-IR.ps1

--- AIO-IR.ps1
+++ AIO-IR.ps1
@@ -1,8 +1,8 @@
 #All-in-one IR script by Biff. Meant for IR use on Windows systems only. YMMV depending on what kind of logging you have enabled#
 #this requires Persistence Sniper be in the same folder as this script. Get it from https://github.com/last-byte/PersistenceSniper/tree/main/PersistenceSniper#
 #set hostname var as HN to keep things "clean"#
-$HN = $env:computername
 Write-host "Forensic Recon Starting"
+$HN = $env:computername
 #get running process network info#
 Write-host "Gathering Process Info"
 Get-NetTCPConnection | select LocalAddress,localport,remoteaddress,remoteport,state,@{name="process";Expression={(get-process -id $_.OwningProcess).ProcessName}}, @{Name="cmdline";Expression={(Get-WmiObject Win32_Process -filter "ProcessId = $($_.OwningProcess)").commandline}} | sort Remoteaddress -Descending | Export-Csv -Path .\$HN-Process-network.csv -NoTypeInformation
@@ -14,13 +14,13 @@ Write-host "Gathering Scheduled Task Info"
 #get scheduled task info#
 schtasks /query /FO CSV /v | convertfrom-csv | where { $_.TaskName -ne "TaskName" } | select "TaskName","Run As User", Author, "Task to Run"| Export-Csv -Path .\$HN-schd-tasks.csv -NoTypeInformation
 #get prefetch so we can have rough history of exes run on the system#
-Write-host "Gathering Prefecth Info"
+Write-host "Gathering Prefetch Info"
 Get-ChildItem C:\Windows\Prefetch | Select Name,LastWriteTime | Export-Csv -Path .\$HN-prefetch.csv -NoTypeInformation
 #get persistence using persistence sniper#
 Write-host "Gathering Persistence Info"
 Import-Module .\PersistenceSniper.psd1
 Find-AllPersistence | Export-Csv -Path .\$HN-persistence.csv -NoTypeInformation
-#get event logs but only evtx that actuall have data. default size of unpopulated log is 69632#
+#get event logs#
 Write-host "Gathering Logs"
 get-childitem C:\Windows\System32\winevt\Logs | where-object {$_.length -gt 69632} | Select -ExpandProperty FullName | Copy-Item -Destination .\logs\
 Write-host "Forensic Recon Complete"
