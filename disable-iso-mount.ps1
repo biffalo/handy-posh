389f691e64d73f4a0a69809c52c863fe16718010
Biff Tannen <biffalo@protonmail.com>
2023-02-08 07:24:44 -0500

    Update disable-iso-mount.ps1
    added vhd mount blocking

--- disable-iso-mount.ps1
+++ disable-iso-mount.ps1
@@ -1,2 +1,4 @@
 #disables mounting of ISOs via file explorer. thanks to https://support.huntress.io/hc/en-us/articles/11477430445587-How-to-disable-ISO-mounting#
 reg add "HKEY_CLASSES_ROOT\Windows.IsoFile\shell\mount" /v "ProgrammaticAccessOnly" /t REG_SZ /d no /f
+#disable VHD/VHDX mounting#
+reg add "HKEY_CLASSES_ROOT\Windows.VhdFile\shell\mount" /v "ProgrammaticAccessOnly" /t REG_SZ /d no /f
