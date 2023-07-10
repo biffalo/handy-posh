214205c2c62d46860509a1b2c0eeb9f23f3b4b72
Biff Tannen <biffalo@protonmail.com>
2023-07-10 10:08:17 -0400

    Update local-account-pw-check.ps1

--- local-account-pw-check.ps1
+++ local-account-pw-check.ps1
@@ -1,1 +1,1 @@
-Get-LocalUser | Where-Object {$_.PasswordRequired -eq $false} | Where-Object {$_.Enabled -eq "True"}
+Get-LocalUser | Where-Object {$_.PasswordRequired -eq $false} | Where-Object {$_.Enabled -eq "True"} > C:\ktemp\localaccount-nopw.txt
