6afd03c9a5cffcb1eb2bc1a219653930ff7415d0
Biff Tannen <biffalo@protonmail.com>
2024-01-10 14:57:32 -0500

    Update smbv1-checker.ps1

--- smbv1-checker.ps1
+++ smbv1-checker.ps1
@@ -1,3 +1,2 @@
 Set-SmbServerConfiguration -AuditSmb1Access $true -Confirm:$false
-Start-Sleep -Seconds 900
 Get-WinEvent -LogName Microsoft-Windows-SMBServer/Audit -FilterXPath "*[System[TimeCreated[timediff(@SystemTime) <= 1209600000]]]" | Where-Object { $_.Id -eq 3000 -or $_.Id -eq 3001 -or $_.Id -eq 3002 } | Select-Object TimeCreated, Id, Message | Format-Table -Wrap
