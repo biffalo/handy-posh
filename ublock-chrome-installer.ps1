54a87923b79c9844b5a70283c774fce27fe9c2c6
Biff Tannen <biffalo@protonmail.com>
2024-08-15 14:36:51 -0400

    Update ublock-chrome-installer.ps1
    updated to account for ublock lite install

--- ublock-chrome-installer.ps1
+++ ublock-chrome-installer.ps1
@@ -1,7 +1,26 @@
-#installs ublock origin in chrome for all users. this is meant for workgroup environments because this is well documented to do with chrome gpo things#
+# Installs uBlock Origin and another Chrome extension in Chrome for all users.
+# This is meant for workgroup environments because this is well documented to do with Chrome GPO things.
+
+# Registry location for Chrome extension policies
 $regLocation = 'Software\Policies\Google\Chrome\ExtensionInstallForcelist'
-$regKey = '1'
-# 'cjpalhdlnbpafiamejdnhcphjbkeiagm' is the Extension ID for ublock origin, easiest way to get this is from the URL of the extension
-$regData = 'cjpalhdlnbpafiamejdnhcphjbkeiagm;https://clients2.google.com/service/update2/crx'
+
+# Extension IDs and update URLs
+$ublockID = 'cjpalhdlnbpafiamejdnhcphjbkeiagm'
+$ublockURL = 'https://clients2.google.com/service/update2/crx'
+$otherExtensionID = 'ddkjiahejlhfcafbddmgiahcphecmpfh'
+$otherExtensionURL = 'https://clients2.google.com/service/update2/crx'
+
+# Registry keys for the extensions
+$ublockKey = '1'
+$otherExtensionKey = '2'
+
+# Registry data (Extension ID and URL)
+$ublockData = "$ublockID;$ublockURL"
+$otherExtensionData = "$otherExtensionID;$otherExtensionURL"
+
+# Create the registry location if it doesn't exist and force the installation of uBlock Origin
 New-Item -Path "HKLM:\$regLocation" -Force
-New-ItemProperty -Path "HKLM:\$regLocation" -Name $regKey -Value $regData -PropertyType STRING -Force
+New-ItemProperty -Path "HKLM:\$regLocation" -Name $ublockKey -Value $ublockData -PropertyType STRING -Force
+
+# Force the installation of the second Chrome extension
+New-ItemProperty -Path "HKLM:\$regLocation" -Name $otherExtensionKey -Value $otherExtensionData -PropertyType STRING -Force
