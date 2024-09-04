1f38bf5a85318337534a66f02826ef68db73b31b
Biff Tannen <biffalo@protonmail.com>
2024-09-04 09:43:26 -0400

    Update Get-DiskSpace.ps1

--- Get-DiskSpace.ps1
+++ Get-DiskSpace.ps1
@@ -1,21 +1,30 @@
-#gets total/used space for each partition in GB#
-$partitions = Get-WmiObject Win32_LogicalDisk | Where-Object {$_.DriveType -eq 3}
+# This script gets you the following info for each partition:
+                                      # Free/Used Disk space
+                                      # Top 10 files by size (exculdes .sys files and /windows/
+# Recurse depth is set to 3 to avoid longer runs
+# Gets total/used space for each partition in GB (internal and external drives)
+$partitions = Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq 3 -or $_.DriveType -eq 2 }
 foreach ($partition in $partitions) {
   $size = "{0:N2}" -f ($partition.Size/1GB)
   $freespace = "{0:N2}" -f ($partition.FreeSpace/1GB)
   $usedspace = "{0:N2}" -f ($size - $freespace)
+Write-Host "==================================="
   Write-Host "Partition: $($partition.DeviceID)"
   Write-Host "Total size: $size GB"
   Write-Host "Free space: $freespace GB"
   Write-Host "Used space: $usedspace GB"
-  #gets the 20 largest files in GB searching in C:\users\ recursivly#
-  Get-ChildItem "C:\Users" -Recurse |
+  Write-Host "Top 10 larest files:"
+
+  # Gets the 20 largest files, excluding /Windows/ directory and .sys files
+  Get-ChildItem "$($partition.DeviceID)\" -Recurse -Depth 3 -ErrorAction SilentlyContinue |
+  Where-Object { 
+    $_.FullName -notlike "*\Windows\*" -and 
+    $_.Extension -ne ".sys" 
+} |
     Sort-Object Length -Descending |
-    Select-Object -First 20 |
-    Format-Table Name, @{Label="Size (GB)"; Expression={("{0:N2}" -f ($_.Length/1GB))}} -Wrap
-  
-  Write-Host
+    Select-Object -First 10 |
+    Format-Table FullName, @{Label="Size (GB)"; Expression={("{0:N2}" -f ($_.Length/1GB))}} -Wrap
+
+  Write-Host "==================================="
 }
-Write-Host "===================================="
-Write-Host "20 largest files full path"
-Get-ChildItem "C:\Users" -Recurse | Sort-Object Length -Descending | Select-Object -First 20 | Format-Table FullName -Wrap
+
