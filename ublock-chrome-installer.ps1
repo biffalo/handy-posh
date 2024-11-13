5b7cc59ddc2fc140cb01a73635b30d800a17afbc
Biff Tannen <biffalo@protonmail.com>
2024-11-13 08:51:51 -0500

    Update ublock-chrome-installer.ps1
    fixed a dumb

--- ublock-chrome-installer.ps1
+++ ublock-chrome-installer.ps1
@@ -1,7 +1,7 @@
 # Installs uBlock Origin in Chrome for all users. Also forces enable v2 manifest to give another year of ublock origin before we switch to lite
 # This is meant for workgroup environments because this is well documented to do with Chrome GPO things.
 # Force enable v2 extensions
-New-Item -Path "HKLM:\SOFTWARE\Policies\Google\Chrome" -Force | Out-Null; New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Google\Chrome" -Name "ExtensionManifestV2Availability" -PropertyType DWord -Value 2 -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Google\Chrome" -Name "ExtensionManifestV2Availability" -PropertyType DWord -Value 2 -Force -ErrorAction SilentlyContinue
 
 # Registry location for Chrome extension policies
 $regLocation = 'Software\Policies\Google\Chrome\ExtensionInstallForcelist'
