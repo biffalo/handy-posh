ea3fe38c8d246581fdd851254c758f4a31c11236
Biff Tannen <biffalo@protonmail.com>
2026-09-02 14:05:18 -0400

    Update ublock-edge-installer.ps1

--- ublock-edge-installer.ps1
+++ ublock-edge-installer.ps1
@@ -1,10 +1,7 @@
-#installs ublock origin in chrome for all users. this is meant for workgroup environments because this is well documented to do with chrome gpo things#
-# add reg key to keep v2 manifest extensions for another year
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "ExtensionManifestV2Availability" -PropertyType DWord -Value 2 -Force -ErrorAction SilentlyContinue
-
+# Installs uBlock Origin Lite in Microsoft Edge for all users.
 $regLocation = 'SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist'
 $regKey = '1'
-# 'cjpalhdlnbpafiamejdnhcphjbkeiagm' is the Extension ID for ublock origin, easiest way to get this is from the URL of the extension
-$regData = 'cjpalhdlnbpafiamejdnhcphjbkeiagm;https://clients2.google.com/service/update2/crx'
+# uBlock Origin Lite Edge Add-ons ID: cimighlppcgcoapaliogpjjdehbnofhn
+$regData = 'cimighlppcgcoapaliogpjjdehbnofhn;https://edge.microsoft.com/extensionwebstorebase/v1/crx'
 New-Item -Path "HKLM:\$regLocation" -Force
 New-ItemProperty -Path "HKLM:\$regLocation" -Name $regKey -Value $regData -PropertyType STRING -Force
